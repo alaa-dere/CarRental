fd9eb77f4a0ed21e71faeac04ec90ea97bb4776e
alaa-dere <alaadere35@gmail.com>
2026-09-15 18:41:23 +0300

    Add password hashing for user registration

--- CarRental.Business/Services/UserService.cs
+++ CarRental.Business/Services/UserService.cs
@@ -2,16 +2,19 @@ using CarRental.Business.Services.Interfaces;
 using CarRental.Data.Context;
 using CarRental.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace CarRental.Business.Services;
 
 public class UserService : IUserService
 {
     private readonly CarRentalDbContext _context;
+    private readonly PasswordHasher<User> _passwordHasher;
 
     public UserService(CarRentalDbContext context)
     {
         _context = context;
+        _passwordHasher = new PasswordHasher<User>();
     }
 
     public async Task<bool> EmailExistsAsync(string email)
@@ -21,6 +24,7 @@ public class UserService : IUserService
 
     public async Task RegisterUserAsync(User user, string password)
     {
+        user.PasswordHash = _passwordHasher.HashPassword(user, password);
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
     }
