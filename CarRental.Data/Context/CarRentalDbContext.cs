48b599501aa0bafdb10ce6ba9010707f87e33858
alaa-dere <alaadere35@gmail.com>
2026-09-15 17:40:40 +0300

    Configure EF Core data context

--- CarRental.Data/Context/CarRentalDbContext.cs
+++ CarRental.Data/Context/CarRentalDbContext.cs
@@ -1,6 +1,15 @@
+using CarRental.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace CarRental.Data.Context;
 
-public class CarRentalDbContext
+public class CarRentalDbContext : DbContext
 {
-    
+    public CarRentalDbContext(DbContextOptions<CarRentalDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
 }
