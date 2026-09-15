0cd00517ef7bdb53a06be0db4870a87ac534c8d2
alaa-dere <alaadere35@gmail.com>
2026-09-15 18:22:46 +0300

    Add registration view model validation

---
+++ CarRental.Web/Models/RegisterViewModel.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRental.Web.Models;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; } = string.Empty;
+
+    [Required]
+    [Phone]
+    public string Phone { get; set; } = string.Empty;
+
+    [DataType(DataType.Date)]
+    public DateTime? DateOfBirth { get; set; }
+
+    [Required]
+    public string AddressLine1 { get; set; } = string.Empty;
+
+    public string? AddressLine2 { get; set; }
+
+    [Required]
+    public string City { get; set; } = string.Empty;
+
+    [Required]
+    public string Country { get; set; } = string.Empty;
+
+    [Required]
+    public string DriverLicenseNumber { get; set; } = string.Empty;
+}
