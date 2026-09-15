2e0fa7214febe35f68307eec8950b1f103874004
alaa-dere <alaadere35@gmail.com>
2026-09-15 22:55:02 +0300

    Add seeded cars and main page

--- CarRental.Data/Context/CarRentalDbContext.cs
+++ CarRental.Data/Context/CarRentalDbContext.cs
@@ -18,5 +18,47 @@ public class CarRentalDbContext : DbContext
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<User>().HasIndex(user => user.Email).IsUnique();
         modelBuilder.Entity<Car>().Property(car => car.PricePerDay).HasPrecision(10, 2);
+        modelBuilder.Entity<Car>().HasData(
+            new Car
+            {
+                Id = 1,
+                Make = "Toyota",
+                Model = "Corolla",
+                Year = 2024,
+                Location = "Nablus",
+                PricePerDay = 40.00m,
+                IsAvailable = true
+            },
+            new Car
+            {
+                Id = 2,
+                Make = "Kia",
+                Model = "Sportage",
+                Year = 2023,
+                Location = "Ramallah",
+                PricePerDay = 65.00m,
+                IsAvailable = true
+            },
+            new Car
+            {
+                Id = 3,
+                Make = "Hyundai",
+                Model = "Elantra",
+                Year = 2022,
+                Location = "Nablus",
+                PricePerDay = 45.00m,
+                IsAvailable = true
+            },
+            new Car
+            {
+                Id = 4,
+                Make = "BMW",
+                Model = "X5",
+                Year = 2024,
+                Location = "Ramallah",
+                PricePerDay = 120.00m,
+                IsAvailable = false
+            }
+        );
     }
 }
---
+++ CarRental.Data/Migrations/20260915182548_SeedCars.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CarRental.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCars : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "IsAvailable", "Location", "Make", "Model", "PricePerDay", "Year" },
+                values: new object[,]
+                {
+                    { 1, true, "Nablus", "Toyota", "Corolla", 40.00m, 2024 },
+                    { 2, true, "Ramallah", "Kia", "Sportage", 65.00m, 2023 },
+                    { 3, true, "Nablus", "Hyundai", "Elantra", 45.00m, 2022 },
+                    { 4, false, "Ramallah", "BMW", "X5", 120.00m, 2024 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Cars",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
