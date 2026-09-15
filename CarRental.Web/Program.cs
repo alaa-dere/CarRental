f038a445ef597d52aa34418ab7ebbbfe3fb5d52d
alaa-dere <alaadere35@gmail.com>
2026-09-15 20:27:55 +0300

    Configure cookie authentication

--- CarRental.Web/Program.cs
+++ CarRental.Web/Program.cs
@@ -2,11 +2,27 @@ using CarRental.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using CarRental.Business.Services;
 using CarRental.Business.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        options.SlidingExpiration = true;
+
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+    });
+builder.Services.AddAuthorization();
 builder.Services.AddDbContext<CarRentalDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CarRentalConnection")));
 builder.Services.AddScoped<IUserService, UserService>();
@@ -25,7 +41,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
