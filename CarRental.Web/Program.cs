80759f4c3dcdd47f8d78a55f1608e90c61732f92
alaa-dere <alaadere35@gmail.com>
2026-09-15 17:52:08 +0300

    Configure SQL Server database connection

--- CarRental.Web/Program.cs
+++ CarRental.Web/Program.cs
@@ -1,7 +1,12 @@
+using CarRental.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CarRentalDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CarRentalConnection")));
 
 var app = builder.Build();
 
